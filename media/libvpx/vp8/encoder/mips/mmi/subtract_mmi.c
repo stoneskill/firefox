f33cbd34899cf867355af194168bfbb4f1337ac1
Zhou Xiaoyong <zhouxiaoyong@loongson.cn>
2016-08-23 12:36:52 +0800

    libvpx/vp8/encoder: loongson optimize vp8_subtract_b_mmi
    Change-Id: I0c962b50634074a86a61f859a460f6cab7ccc4c7

--- media/libvpx/vp8/encoder/mips/mmi/subtract_mmi.c
+++ media/libvpx/vp8/encoder/mips/mmi/subtract_mmi.c
@@ -16,6 +16,108 @@
 
 void vp8_subtract_b_mmi(BLOCK *be, BLOCKD *bd, int pitch)
 {
+#if 1
+    unsigned char *src_ptr = (*(be->base_src) + be->src);
+    short *diff_ptr = be->src_diff;
+    unsigned char *pred_ptr = bd->predictor;
+    int src_stride = be->src_stride;
+    double ftmp[12];
+#if _MIPS_SIM == _ABIO32
+    uint32_t tmp[1];
+#endif
+
+    __asm__ volatile (
+        "xor        %[ftmp0],   %[ftmp0],           %[ftmp0]        \n\t"
+#if _MIPS_SIM == _ABIO32
+        "ulw        %[tmp0],    0x00(%[src_prt])                    \n\t"
+        "mtc1       %[tmp0],    %[ftmp1]                            \n\t"
+        "ulw        %[tmp0],    0x00(%[pred_prt])                   \n\t"
+        "mtc1       %[tmp0],    %[ftmp2]                            \n\t"
+#else
+        "gslwlc1    %[ftmp1],   0x03(%[src_ptr])                    \n\t"
+        "gslwrc1    %[ftmp1],   0x00(%[src_ptr])                    \n\t"
+        "gslwlc1    %[ftmp2],   0x03(%[pred_ptr])                   \n\t"
+        "gslwrc1    %[ftmp2],   0x00(%[pred_ptr])                   \n\t"
+#endif
+        PTR_ADDU   "%[src_ptr], %[src_ptr],         %[src_stride]   \n\t"
+        PTR_ADDU   "%[pred_ptr],%[pred_ptr],        %[pitch]        \n\t"
+#if _MIPS_SIM == _ABIO32
+        "ulw        %[tmp0],    0x00(%[src_prt])                    \n\t"
+        "mtc1       %[tmp0],    %[ftmp3]                            \n\t"
+        "ulw        %[tmp0],    0x00(%[pred_prt])                   \n\t"
+        "mtc1       %[tmp0],    %[ftmp4]                            \n\t"
+#else
+        "gslwlc1    %[ftmp3],   0x03(%[src_ptr])                    \n\t"
+        "gslwrc1    %[ftmp3],   0x00(%[src_ptr])                    \n\t"
+        "gslwlc1    %[ftmp4],   0x03(%[pred_ptr])                   \n\t"
+        "gslwrc1    %[ftmp4],   0x00(%[pred_ptr])                   \n\t"
+#endif
+        PTR_ADDU   "%[src_ptr], %[src_ptr],         %[src_stride]   \n\t"
+        PTR_ADDU   "%[pred_ptr],%[pred_ptr],        %[pitch]        \n\t"
+#if _MIPS_SIM == _ABIO32
+        "ulw        %[tmp0],    0x00(%[src_prt])                    \n\t"
+        "mtc1       %[tmp0],    %[ftmp5]                            \n\t"
+        "ulw        %[tmp0],    0x00(%[pred_prt])                   \n\t"
+        "mtc1       %[tmp0],    %[ftmp6]                            \n\t"
+#else
+        "gslwlc1    %[ftmp5],   0x03(%[src_ptr])                    \n\t"
+        "gslwrc1    %[ftmp5],   0x00(%[src_ptr])                    \n\t"
+        "gslwlc1    %[ftmp6],   0x03(%[pred_ptr])                   \n\t"
+        "gslwrc1    %[ftmp6],   0x00(%[pred_ptr])                   \n\t"
+#endif
+        PTR_ADDU   "%[src_ptr], %[src_ptr],         %[src_stride]   \n\t"
+        PTR_ADDU   "%[pred_ptr],%[pred_ptr],        %[pitch]        \n\t"
+#if _MIPS_SIM == _ABIO32
+        "ulw        %[tmp0],    0x00(%[src_prt])                    \n\t"
+        "mtc1       %[tmp0],    %[ftmp7]                            \n\t"
+        "ulw        %[tmp0],    0x00(%[pred_prt])                   \n\t"
+        "mtc1       %[tmp0],    %[ftmp8]                            \n\t"
+#else
+        "gslwlc1    %[ftmp7],   0x03(%[src_ptr])                    \n\t"
+        "gslwrc1    %[ftmp7],   0x00(%[src_ptr])                    \n\t"
+        "gslwlc1    %[ftmp8],   0x03(%[pred_ptr])                   \n\t"
+        "gslwrc1    %[ftmp8],   0x00(%[pred_ptr])                   \n\t"
+#endif
+
+        "punpcklbh  %[ftmp9],   %[ftmp1],           %[ftmp0]        \n\t"
+        "punpcklbh  %[ftmp10],  %[ftmp2],           %[ftmp0]        \n\t"
+        "psubh      %[ftmp11],  %[ftmp9],           %[ftmp10]       \n\t"
+        "gssdlc1    %[ftmp11],  0x07(%[diff_ptr])                   \n\t"
+        "gssdrc1    %[ftmp11],  0x00(%[diff_ptr])                   \n\t"
+        PTR_ADDU   "%[diff_ptr],%[diff_ptr],        %[pitch_x2]     \n\t"
+        "punpcklbh  %[ftmp9],   %[ftmp3],           %[ftmp0]        \n\t"
+        "punpcklbh  %[ftmp10],  %[ftmp4],           %[ftmp0]        \n\t"
+        "psubh      %[ftmp11],  %[ftmp9],           %[ftmp10]       \n\t"
+        "gssdlc1    %[ftmp11],  0x07(%[diff_ptr])                   \n\t"
+        "gssdrc1    %[ftmp11],  0x00(%[diff_ptr])                   \n\t"
+        PTR_ADDU   "%[diff_ptr],%[diff_ptr],        %[pitch_x2]     \n\t"
+        "punpcklbh  %[ftmp9],   %[ftmp5],           %[ftmp0]        \n\t"
+        "punpcklbh  %[ftmp10],  %[ftmp6],           %[ftmp0]        \n\t"
+        "psubh      %[ftmp11],  %[ftmp9],           %[ftmp10]       \n\t"
+        "gssdlc1    %[ftmp11],  0x07(%[diff_ptr])                   \n\t"
+        "gssdrc1    %[ftmp11],  0x00(%[diff_ptr])                   \n\t"
+        PTR_ADDU   "%[diff_ptr],%[diff_ptr],        %[pitch_x2]     \n\t"
+        "punpcklbh  %[ftmp9],   %[ftmp7],           %[ftmp0]        \n\t"
+        "punpcklbh  %[ftmp10],  %[ftmp8],           %[ftmp0]        \n\t"
+        "psubh      %[ftmp11],  %[ftmp9],           %[ftmp10]       \n\t"
+        "gssdlc1    %[ftmp11],  0x07(%[diff_ptr])                   \n\t"
+        "gssdrc1    %[ftmp11],  0x00(%[diff_ptr])                   \n\t"
+        : [ftmp0]"=&f"(ftmp[0]),            [ftmp1]"=&f"(ftmp[1]),
+          [ftmp2]"=&f"(ftmp[2]),            [ftmp3]"=&f"(ftmp[3]),
+          [ftmp4]"=&f"(ftmp[4]),            [ftmp5]"=&f"(ftmp[5]),
+          [ftmp6]"=&f"(ftmp[6]),            [ftmp7]"=&f"(ftmp[7]),
+          [ftmp8]"=&f"(ftmp[8]),            [ftmp9]"=&f"(ftmp[9]),
+          [ftmp10]"=&f"(ftmp[10]),          [ftmp11]"=&f"(ftmp[11]),
+#if _MIPS_SIM == _ABIO32
+          [tmp0]"=&r"(tmp[0]),
+#endif
+          [src_ptr]"+&r"(src_ptr),          [pred_ptr]"+&r"(pred_ptr),
+          [diff_ptr]"+&r"(diff_ptr)
+        : [src_stride]"r"((mips_reg)src_stride),
+          [pitch]"r"((mips_reg)pitch),      [pitch_x2]"r"((mips_reg)(pitch*2))
+        : "memory"
+    );
+#else
     unsigned char *src_ptr = (*(be->base_src) + be->src);
     short *diff_ptr = be->src_diff;
     unsigned char *pred_ptr = bd->predictor;
@@ -33,6 +135,7 @@ void vp8_subtract_b_mmi(BLOCK *be, BLOCKD *bd, int pitch)
         pred_ptr += pitch;
         src_ptr  += src_stride;
     }
+#endif
 }
 
 void vp8_subtract_mbuv_mmi(short *diff, unsigned char *usrc, unsigned char *vsrc,
