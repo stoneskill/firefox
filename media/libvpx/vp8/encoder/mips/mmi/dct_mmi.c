921034692e91a9b75432347e5b4eadbad2016c32
ZhouXiaoyong <zhouxiaoyong@loongson.cn>
2016-08-23 09:39:14 +0800

    libvpx/vp8/encoder: loongson optimize vp8_short_walsh4x4_mmi
    Change-Id: I225851148ec0460184589f39dce8603719794c60

--- media/libvpx/vp8/encoder/mips/mmi/dct_mmi.c
+++ media/libvpx/vp8/encoder/mips/mmi/dct_mmi.c
@@ -37,7 +37,7 @@
         "punpcklwd "#f2", "#f10", "#f14"         \n\t" \
         "punpckhwd "#f4", "#f10", "#f14"         \n\t" \
         "punpcklwd "#f6", "#f12", "#f16"         \n\t" \
-        "punpckhwd "#f8", "#f12", "#f16"         \n\t" \
+        "punpckhwd "#f8", "#f12", "#f16"         \n\t"
 
 void vp8_short_fdct4x4_mmi(short *input, short *output, int pitch)
 {
@@ -388,54 +388,298 @@ void vp8_short_fdct8x4_mmi(short *input, short *output, int pitch)
 
 void vp8_short_walsh4x4_mmi(short *input, short *output, int pitch)
 {
-    int i;
+#if 1
+    double ftmp[13];
+    uint32_t tmp[1];
+    DECLARE_ALIGNED(8, const uint64_t, ff_ph_01)   = {0x0001000100010001ULL};
+    DECLARE_ALIGNED(8, const uint64_t, ff_pw_01)   = {0x0000000100000001ULL};
+    DECLARE_ALIGNED(8, const uint64_t, ff_pw_03)   = {0x0000000300000003ULL};
+    DECLARE_ALIGNED(8, const uint64_t, ff_pw_mask) = {0x0001000000010000ULL};
+
+    __asm__ volatile (
+        "li         %[tmp0],    0x02                                \n\t"
+        "xor        %[ftmp0],   %[ftmp0],       %[ftmp0]            \n\t"
+        "mtc1       %[tmp0],    %[ftmp11]                           \n\t"
+
+        "gsldlc1    %[ftmp1],   0x07(%[ip])                         \n\t"
+        "gsldrc1    %[ftmp1],   0x00(%[ip])                         \n\t"
+        PTR_ADDU   "%[ip],      %[ip],          %[pitch]            \n\t"
+        "gsldlc1    %[ftmp2],   0x07(%[ip])                         \n\t"
+        "gsldrc1    %[ftmp2],   0x00(%[ip])                         \n\t"
+        PTR_ADDU   "%[ip],      %[ip],          %[pitch]            \n\t"
+        "gsldlc1    %[ftmp3],   0x07(%[ip])                         \n\t"
+        "gsldrc1    %[ftmp3],   0x00(%[ip])                         \n\t"
+        PTR_ADDU   "%[ip],      %[ip],          %[pitch]            \n\t"
+        "gsldlc1    %[ftmp4],   0x07(%[ip])                         \n\t"
+        "gsldrc1    %[ftmp4],   0x00(%[ip])                         \n\t"
+
+        TRANSPOSE_4H(%[ftmp1], %[ftmp2], %[ftmp3], %[ftmp4],
+                     %[ftmp5], %[ftmp6], %[ftmp7], %[ftmp8],
+                     %[ftmp9], %[tmp0],  %[ftmp0], %[ftmp10])
+
+        "psllh      %[ftmp1],   %[ftmp1],       %[ftmp11]           \n\t"
+        "psllh      %[ftmp2],   %[ftmp2],       %[ftmp11]           \n\t"
+        "psllh      %[ftmp3],   %[ftmp3],       %[ftmp11]           \n\t"
+        "psllh      %[ftmp4],   %[ftmp4],       %[ftmp11]           \n\t"
+        // a
+        "paddh      %[ftmp5],   %[ftmp1],       %[ftmp3]            \n\t"
+        // d
+        "paddh      %[ftmp6],   %[ftmp2],       %[ftmp4]            \n\t"
+        // c
+        "psubh      %[ftmp7],   %[ftmp2],       %[ftmp4]            \n\t"
+        // b
+        "psubh      %[ftmp8],   %[ftmp1],       %[ftmp3]            \n\t"
+
+        // a + d
+        "paddh      %[ftmp1],   %[ftmp5],       %[ftmp6]            \n\t"
+        // b + c
+        "paddh      %[ftmp2],   %[ftmp8],       %[ftmp7]            \n\t"
+        // b - c
+        "psubh      %[ftmp3],   %[ftmp8],       %[ftmp7]            \n\t"
+        // a - d
+        "psubh      %[ftmp4],   %[ftmp5],       %[ftmp6]            \n\t"
+
+        "dmtc1      %[ff_ph_01],%[ftmp11]                           \n\t"
+        "pcmpeqh    %[ftmp6],   %[ftmp5],       %[ftmp0]            \n\t"
+        "paddh      %[ftmp6],   %[ftmp6],       %[ftmp11]           \n\t"
+        "paddh      %[ftmp1],   %[ftmp1],       %[ftmp6]            \n\t"
+
+        TRANSPOSE_4H(%[ftmp1], %[ftmp2], %[ftmp3], %[ftmp4],
+                     %[ftmp5], %[ftmp6], %[ftmp7], %[ftmp8],
+                     %[ftmp9], %[tmp0],  %[ftmp0], %[ftmp10])
+
+        // op[2], op[0]
+        "pmaddhw    %[ftmp5],   %[ftmp1],       %[ff_pw_01]         \n\t"
+        // op[3], op[1]
+        "pmaddhw    %[ftmp1],   %[ftmp1],       %[ff_pw_mask]       \n\t"
+
+        // op[6], op[4]
+        "pmaddhw    %[ftmp6],   %[ftmp2],       %[ff_pw_01]         \n\t"
+        // op[7], op[5]
+        "pmaddhw    %[ftmp2],   %[ftmp2],       %[ff_pw_mask]       \n\t"
+
+        // op[10], op[8]
+        "pmaddhw    %[ftmp7],   %[ftmp3],       %[ff_pw_01]         \n\t"
+        // op[11], op[9]
+        "pmaddhw    %[ftmp3],   %[ftmp3],       %[ff_pw_mask]       \n\t"
+
+        // op[14], op[12]
+        "pmaddhw    %[ftmp8],   %[ftmp4],       %[ff_pw_01]         \n\t"
+        // op[15], op[13]
+        "pmaddhw    %[ftmp4],   %[ftmp4],       %[ff_pw_mask]       \n\t"
+
+        // a1, a3
+        "paddw      %[ftmp9],   %[ftmp5],       %[ftmp7]            \n\t"
+        // d1, d3
+        "paddw      %[ftmp10],  %[ftmp6],       %[ftmp8]            \n\t"
+        // c1, c3
+        "psubw      %[ftmp11],  %[ftmp6],       %[ftmp8]            \n\t"
+        // b1, b3
+        "psubw      %[ftmp12],  %[ftmp5],       %[ftmp7]            \n\t"
+
+        // a1 + d1, a3 + d3
+        "paddw      %[ftmp5],   %[ftmp9],       %[ftmp10]           \n\t"
+        // b1 + c1, b3 + c3
+        "paddw      %[ftmp6],   %[ftmp12],      %[ftmp11]           \n\t"
+        // b1 - c1, b3 - c3
+        "psubw      %[ftmp7],   %[ftmp12],      %[ftmp11]           \n\t"
+        // a1 - d1, a3 - d3
+        "psubw      %[ftmp8],   %[ftmp9],       %[ftmp10]           \n\t"
+
+        // a2, a4
+        "paddw      %[ftmp9],   %[ftmp1],       %[ftmp3]            \n\t"
+        // d2, d4
+        "paddw      %[ftmp10],  %[ftmp2],       %[ftmp4]            \n\t"
+        // c2, c4
+        "psubw      %[ftmp11],  %[ftmp2],       %[ftmp4]            \n\t"
+        // b2, b4
+        "psubw      %[ftmp12],  %[ftmp1],       %[ftmp3]            \n\t"
+
+        // a2 + d2, a4 + d4
+        "paddw      %[ftmp1],   %[ftmp9],       %[ftmp10]           \n\t"
+        // b2 + c2, b4 + c4
+        "paddw      %[ftmp2],   %[ftmp12],      %[ftmp11]           \n\t"
+        // b2 - c2, b4 - c4
+        "psubw      %[ftmp3],   %[ftmp12],      %[ftmp11]           \n\t"
+        // a2 - d2, a4 - d4
+        "psubw      %[ftmp4],   %[ftmp9],       %[ftmp10]           \n\t"
+
+        "li         %[tmp0],    0x03                                \n\t"
+        "mtc1       %[tmp0],    %[ftmp11]                           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp1]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp1],   %[ftmp1],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp1],   %[ftmp1],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp1],   %[ftmp1],       %[ftmp11]           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp2]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp2],   %[ftmp2],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp2],   %[ftmp2],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp2],   %[ftmp2],       %[ftmp11]           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp3]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp3],   %[ftmp3],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp3],   %[ftmp3],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp3],   %[ftmp3],       %[ftmp11]           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp4]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp4],   %[ftmp4],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp4],   %[ftmp4],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp4],   %[ftmp4],       %[ftmp11]           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp5]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp5],   %[ftmp5],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp5],   %[ftmp5],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp5],   %[ftmp5],       %[ftmp11]           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp6]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp6],   %[ftmp6],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp6],   %[ftmp6],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp6],   %[ftmp6],       %[ftmp11]           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp7]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp7],   %[ftmp7],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp7],   %[ftmp7],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp7],   %[ftmp7],       %[ftmp11]           \n\t"
+
+        "pcmpgtw    %[ftmp9],   %[ftmp0],       %[ftmp8]            \n\t"
+        "and        %[ftmp9],   %[ftmp9],       %[ff_pw_01]         \n\t"
+        "paddw      %[ftmp8],   %[ftmp8],       %[ftmp9]            \n\t"
+        "paddw      %[ftmp8],   %[ftmp8],       %[ff_pw_03]         \n\t"
+        "psraw      %[ftmp8],   %[ftmp8],       %[ftmp11]           \n\t"
+
+        "packsswh   %[ftmp1],   %[ftmp1],       %[ftmp5]            \n\t"
+        "packsswh   %[ftmp2],   %[ftmp2],       %[ftmp6]            \n\t"
+        "packsswh   %[ftmp3],   %[ftmp3],       %[ftmp7]            \n\t"
+        "packsswh   %[ftmp4],   %[ftmp4],       %[ftmp8]            \n\t"
+
+        "li         %[tmp0],    0x72                                \n\t"
+        "mtc1       %[tmp0],    %[ftmp11]                           \n\t"
+        "pshufh     %[ftmp1],   %[ftmp1],       %[ftmp11]           \n\t"
+        "pshufh     %[ftmp2],   %[ftmp2],       %[ftmp11]           \n\t"
+        "pshufh     %[ftmp3],   %[ftmp3],       %[ftmp11]           \n\t"
+        "pshufh     %[ftmp4],   %[ftmp4],       %[ftmp11]           \n\t"
+
+        "gssdlc1    %[ftmp1],   0x07(%[op])                         \n\t"
+        "gssdrc1    %[ftmp1],   0x00(%[op])                         \n\t"
+        "gssdlc1    %[ftmp2],   0x0f(%[op])                         \n\t"
+        "gssdrc1    %[ftmp2],   0x08(%[op])                         \n\t"
+        "gssdlc1    %[ftmp3],   0x17(%[op])                         \n\t"
+        "gssdrc1    %[ftmp3],   0x10(%[op])                         \n\t"
+        "gssdlc1    %[ftmp4],   0x1f(%[op])                         \n\t"
+        "gssdrc1    %[ftmp4],   0x18(%[op])                         \n\t"
+        : [ftmp0]"=&f"(ftmp[0]),            [ftmp1]"=&f"(ftmp[1]),
+          [ftmp2]"=&f"(ftmp[2]),            [ftmp3]"=&f"(ftmp[3]),
+          [ftmp4]"=&f"(ftmp[4]),            [ftmp5]"=&f"(ftmp[5]),
+          [ftmp6]"=&f"(ftmp[6]),            [ftmp7]"=&f"(ftmp[7]),
+          [ftmp8]"=&f"(ftmp[8]),            [ftmp9]"=&f"(ftmp[9]),
+          [ftmp10]"=&f"(ftmp[10]),          [ftmp11]"=&f"(ftmp[11]),
+          [ftmp12]"=&f"(ftmp[12]),
+          [tmp0]"=&r"(tmp[0]),
+          [ip]"+&r"(input)
+        : [op]"r"(output),
+          [ff_pw_01]"f"(ff_pw_01),          [pitch]"r"((mips_reg)pitch),
+          [ff_pw_03]"f"(ff_pw_03),          [ff_pw_mask]"f"(ff_pw_mask),
+          [ff_ph_01]"r"(ff_ph_01)
+        : "memory"
+    );
+#else
     int a1, b1, c1, d1;
     int a2, b2, c2, d2;
+    int a3, b3, c3, d3;
+    int a4, b4, c4, d4;
     short *ip = input;
     short *op = output;
 
+    pitch = pitch / 2;
 
-    for (i = 0; i < 4; i++)
-    {
-        a1 = ((ip[0] + ip[2]) * 4);
-        d1 = ((ip[1] + ip[3]) * 4);
-        c1 = ((ip[1] - ip[3]) * 4);
-        b1 = ((ip[0] - ip[2]) * 4);
+    // stage 1
+    a1 = ((ip[0          ] + ip[2          ]) * 4);
+    a2 = ((ip[0 + pitch  ] + ip[2 + pitch  ]) * 4);
+    a3 = ((ip[0 + pitch*2] + ip[2 + pitch*2]) * 4);
+    a4 = ((ip[0 + pitch*3] + ip[2 + pitch*3]) * 4);
 
-        op[0] = a1 + d1 + (a1!=0);
-        op[1] = b1 + c1;
-        op[2] = b1 - c1;
-        op[3] = a1 - d1;
-        ip += pitch / 2;
-        op += 4;
-    }
+    d1 = ((ip[1          ] + ip[3          ]) * 4);
+    d2 = ((ip[1 + pitch  ] + ip[3 + pitch  ]) * 4);
+    d3 = ((ip[1 + pitch*2] + ip[3 + pitch*2]) * 4);
+    d4 = ((ip[1 + pitch*3] + ip[3 + pitch*3]) * 4);
 
-    ip = output;
-    op = output;
+    c1 = ((ip[1          ] - ip[3          ]) * 4);
+    c2 = ((ip[1 + pitch  ] - ip[3 + pitch  ]) * 4);
+    c3 = ((ip[1 + pitch*2] - ip[3 + pitch*2]) * 4);
+    c4 = ((ip[1 + pitch*3] - ip[3 + pitch*3]) * 4);
 
-    for (i = 0; i < 4; i++)
-    {
-        a1 = ip[0] + ip[8];
-        d1 = ip[4] + ip[12];
-        c1 = ip[4] - ip[12];
-        b1 = ip[0] - ip[8];
+    b1 = ((ip[0          ] - ip[2          ]) * 4);
+    b2 = ((ip[0 + pitch  ] - ip[2 + pitch  ]) * 4);
+    b3 = ((ip[0 + pitch*2] - ip[2 + pitch*2]) * 4);
+    b4 = ((ip[0 + pitch*3] - ip[2 + pitch*3]) * 4);
 
-        a2 = a1 + d1;
-        b2 = b1 + c1;
-        c2 = b1 - c1;
-        d2 = a1 - d1;
+    op[ 0] = a1 + d1 + (a1!=0);
+    op[ 4] = a2 + d2 + (a2!=0);
+    op[ 8] = a3 + d3 + (a3!=0);
+    op[12] = a4 + d4 + (a4!=0);
 
-        a2 += a2<0;
-        b2 += b2<0;
-        c2 += c2<0;
-        d2 += d2<0;
+    op[ 1] = b1 + c1;
+    op[ 5] = b2 + c2;
+    op[ 9] = b3 + c3;
+    op[13] = b4 + c4;
 
-        op[0] = (a2+3) >> 3;
-        op[4] = (b2+3) >> 3;
-        op[8] = (c2+3) >> 3;
-        op[12]= (d2+3) >> 3;
+    op[ 2] = b1 - c1;
+    op[ 6] = b2 - c2;
+    op[10] = b3 - c3;
+    op[14] = b4 - c4;
 
-        ip++;
-        op++;
-    }
+    op[ 3] = a1 - d1;
+    op[ 7] = a2 - d2;
+    op[11] = a3 - d3;
+    op[15] = a4 - d4;
+
+    // stage 2
+    a1 = op[0] + op[ 8];
+    a2 = op[1] + op[ 9];
+    a3 = op[2] + op[10];
+    a4 = op[3] + op[11];
+
+    d1 = op[4] + op[12];
+    d2 = op[5] + op[13];
+    d3 = op[6] + op[14];
+    d4 = op[7] + op[15];
+
+    c1 = op[4] - op[12];
+    c2 = op[5] - op[13];
+    c3 = op[6] - op[14];
+    c4 = op[7] - op[15];
+
+    b1 = op[0] - op[ 8];
+    b2 = op[1] - op[ 9];
+    b3 = op[2] - op[10];
+    b4 = op[3] - op[11];
+
+    op[ 0] = (a1 + d1 + ((a1 + d1)<0) + 3) >> 3;
+    op[ 1] = (a2 + d2 + ((a2 + d2)<0) + 3) >> 3;
+    op[ 2] = (a3 + d3 + ((a3 + d3)<0) + 3) >> 3;
+    op[ 3] = (a4 + d4 + ((a4 + d4)<0) + 3) >> 3;
+
+    op[ 4] = (b1 + c1 + ((b1 + c1)<0) + 3) >> 3;
+    op[ 5] = (b2 + c2 + ((b2 + c2)<0) + 3) >> 3;
+    op[ 6] = (b3 + c3 + ((b3 + c3)<0) + 3) >> 3;
+    op[ 7] = (b4 + c4 + ((b4 + c4)<0) + 3) >> 3;
+
+    op[ 8] = (b1 - c1 + ((b1 - c1)<0) + 3) >> 3;
+    op[ 9] = (b2 - c2 + ((b2 - c2)<0) + 3) >> 3;
+    op[10] = (b3 - c3 + ((b3 - c3)<0) + 3) >> 3;
+    op[11] = (b4 - c4 + ((b4 - c4)<0) + 3) >> 3;
+
+    op[12] = (a1 - d1 + ((a1 - d1)<0) + 3) >> 3;
+    op[13] = (a2 - d2 + ((a2 - d2)<0) + 3) >> 3;
+    op[14] = (a3 - d3 + ((a3 - d3)<0) + 3) >> 3;
+    op[15] = (a4 - d4 + ((a4 - d4)<0) + 3) >> 3;
+#endif
 }
